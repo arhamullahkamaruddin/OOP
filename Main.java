6ba870b47002ef28cd96cbc5c8c11a9ed185f0f6
Arhamullah Kamaruddin <123912972+arhamullahkamaruddin@users.noreply.github.com>
2024-03-03 20:57:33 +0800

    Class Main

---
+++ Main.java
@@ -0,0 +1,635 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tugas1;
+
+/**
+ *
+ * @author Asus
+ */
+public class Main {
+    public static void main(String[] args) {
+        Kucing kucing1 = new Kucing();
+        Kucing kucing2 = new Kucing();
+        Kucing kucing3 = new Kucing();
+        Kucing kucing4 = new Kucing();
+        Kucing kucing5 = new Kucing();
+        Kucing kucing6 = new Kucing();
+        Kucing kucing7 = new Kucing();
+        Kucing kucing8 = new Kucing();
+        Kucing kucing9 = new Kucing();
+        Kucing kucing10 = new Kucing();
+        
+        kucing1.jenis = "Felis Nigripes";
+        kucing1.warna = "Hitam";
+        kucing1.umur = 3;
+        kucing1.printStates();
+        kucing1.makan();
+        
+        kucing2.jenis = "Persia";
+        kucing2.warna = "Putih";
+        kucing2.umur = 2;
+        kucing2.printStates();
+        kucing2.lari();
+        
+        kucing3.jenis = "Maine Coon";
+        kucing3.warna = "Abu-abu";
+        kucing3.umur = 3;
+        kucing3.printStates();
+        kucing3.tidur();
+        
+        kucing4.jenis = "Scottish Fold";
+        kucing4.warna = "Oren";
+        kucing4.umur = 2;
+        kucing4.printStates();
+        kucing4.lari();
+        
+        kucing5.jenis = "Bengal";
+        kucing5.warna = "Coklat";
+        kucing5.umur = 1;
+        kucing5.printStates();
+        kucing5.tidur();
+        
+        kucing6.jenis = "Ragdol";
+        kucing6.warna = "Hitam";
+        kucing6.umur = 2;
+        kucing6.printStates();
+        kucing6.makan();
+        
+        kucing7.jenis = "Sphynx";
+        kucing7.warna = "Coklat";
+        kucing7.umur = 4;
+        kucing7.printStates();
+        kucing7.tidur();
+        
+        kucing8.jenis = "British Shorthair";
+        kucing8.warna = "Putih";
+        kucing8.umur = 2;
+        kucing8.printStates();
+        kucing8.tidur();
+        
+        kucing9.jenis = "American Shorthair";
+        kucing9.warna = "Hitam";
+        kucing9.umur = 1;
+        kucing9.printStates();
+        kucing9.lari();
+        
+        kucing10.jenis = "Russian Blue";
+        kucing10.warna = "Abu-abu";
+        kucing10.umur = 3;
+        kucing10.printStates();
+        kucing10.makan();
+        
+        Mahasiswa mahasiswa1 = new Mahasiswa();
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        Mahasiswa mahasiswa3 = new Mahasiswa();
+        Mahasiswa mahasiswa4 = new Mahasiswa();
+        Mahasiswa mahasiswa5 = new Mahasiswa();
+        Mahasiswa mahasiswa6 = new Mahasiswa();
+        Mahasiswa mahasiswa7 = new Mahasiswa();
+        Mahasiswa mahasiswa8 = new Mahasiswa();
+        Mahasiswa mahasiswa9 = new Mahasiswa();
+        Mahasiswa mahasiswa10 = new Mahasiswa();
+        
+        mahasiswa1.nama = "Arham";
+        mahasiswa1.nim = "D0223002";
+        mahasiswa1.printStates();
+        mahasiswa1.belajar();
+        
+        mahasiswa2.nama = "Miky";
+        mahasiswa2.nim = "D0223001";
+        mahasiswa2.printStates();
+        mahasiswa2.kerjaTugas();
+        
+        mahasiswa3.nama = "Faisal";
+        mahasiswa3.nim = "D0223003";
+        mahasiswa3.printStates();
+        mahasiswa3.belajar();
+        
+        mahasiswa4.nama = "Reski";
+        mahasiswa4.nim = "D0223004";
+        mahasiswa4.printStates();
+        mahasiswa4.kerjaTugas();
+        
+        mahasiswa5.nama = "Suardi";
+        mahasiswa5.nim = "D0223005";
+        mahasiswa5.printStates();
+        mahasiswa5.belajar();
+        
+        mahasiswa6.nama = "Aiman";
+        mahasiswa6.nim = "D0223006";
+        mahasiswa6.printStates();
+        mahasiswa6.kerjaTugas();
+        
+        mahasiswa7.nama = "Shabir";
+        mahasiswa7.nim = "D0223005";
+        mahasiswa7.printStates();
+        mahasiswa7.belajar();
+        
+        mahasiswa8.nama = "Raffi";
+        mahasiswa8.nim = "D0223006";
+        mahasiswa8.printStates();
+        mahasiswa8.kerjaTugas();
+        
+        mahasiswa9.nama = "Alvin";
+        mahasiswa9.nim = "D0223009";
+        mahasiswa9.printStates();
+        mahasiswa9.belajar();
+        
+        mahasiswa10.nama = "Aril";
+        mahasiswa10.nim = "D0223010";
+        mahasiswa10.printStates();
+        mahasiswa10.kerjaTugas();
+        
+        Burung burung1 = new Burung();
+        Burung burung2 = new Burung();
+        Burung burung3 = new Burung();
+        Burung burung4 = new Burung();
+        Burung burung5 = new Burung();
+        Burung burung6 = new Burung();
+        Burung burung7 = new Burung();
+        Burung burung8 = new Burung();
+        Burung burung9 = new Burung();
+        Burung burung10 = new Burung();
+        
+        burung1.jenis = "Gereja";
+        burung1.warna = "Coklat";
+        burung1.printStates();
+        burung1.terbang();
+        
+        burung2.jenis = "Elang";
+        burung2.warna = "Coklat";
+        burung2.printStates();
+        burung2.makan();
+        
+        burung3.jenis = "Kutilang";
+        burung3.warna = "Kuning";
+        burung3.printStates();
+        burung3.terbang();
+        
+        burung4.jenis = "Pipit";
+        burung4.warna = "Coklat";
+        burung4.printStates();
+        burung4.makan();
+        
+        burung5.jenis = "Nuri";
+        burung5.warna = "Merah";
+        burung5.printStates();
+        burung5.terbang();
+        
+        burung6.jenis = "Gagak";
+        burung6.warna = "Hitam";
+        burung6.printStates();
+        burung6.makan();
+        
+        burung7.jenis = "Nuri";
+        burung7.warna = "Biru";
+        burung7.printStates();
+        burung7.terbang();
+        
+        burung8.jenis = "Falcon";
+        burung8.warna = "Hitam";
+        burung8.printStates();
+        burung8.makan();
+        
+        burung9.jenis = "Elang Botak";
+        burung9.warna = "Coklat";
+        burung9.printStates();
+        burung9.terbang();
+        
+        burung10.jenis = "Elang Emas";
+        burung10.warna = "Putih";
+        burung10.printStates();
+        burung10.makan();
+        
+        Ular ular1 = new Ular();
+        Ular ular2 = new Ular();
+        Ular ular3 = new Ular();
+        Ular ular4 = new Ular();
+        Ular ular5 = new Ular();
+        Ular ular6 = new Ular();
+        Ular ular7 = new Ular();
+        Ular ular8 = new Ular();
+        Ular ular9 = new Ular();
+        Ular ular10 = new Ular();
+        
+        ular1.jenis = "Anaconda";
+        ular1.ukuran = 12;
+        ular1.printStates();
+        ular1.makan();
+        
+        ular2.jenis = "Sawah";
+        ular2.ukuran = 8;
+        ular2.printStates();
+        ular2.merayap();
+        
+        ular3.jenis = "Anaconda";
+        ular3.ukuran = 7;
+        ular3.printStates();
+        ular3.makan();
+        
+        ular4.jenis = "Cobra";
+        ular4.ukuran = 5;
+        ular4.printStates();
+        ular4.merayap();
+        
+        ular5.jenis = "Daun";
+        ular5.ukuran = 1;
+        ular5.printStates();
+        ular5.makan();
+        
+        ular6.jenis = "Laut";
+        ular6.ukuran = 1;
+        ular6.printStates();
+        ular6.merayap();
+       
+        ular7.jenis = "Cobra";
+        ular7.ukuran = 3;
+        ular7.printStates();
+        ular7.makan();
+        
+        ular8.jenis = "Sawah";
+        ular8.ukuran = 3;
+        ular8.printStates();
+        ular8.merayap();
+       
+        ular9.jenis = "Laut";
+        ular9.ukuran = 2;
+        ular9.printStates();
+        ular9.makan();
+        
+        ular10.jenis = "Anaconda";
+        ular10.ukuran = 5;
+        ular10.printStates();
+        ular10.merayap();
+        
+        Sapi sapi1 = new Sapi();
+        Sapi sapi2 = new Sapi();
+        Sapi sapi3 = new Sapi();
+        Sapi sapi4 = new Sapi();
+        Sapi sapi5 = new Sapi();
+        Sapi sapi6 = new Sapi();
+        Sapi sapi7 = new Sapi();
+        Sapi sapi8 = new Sapi();
+        Sapi sapi9 = new Sapi();
+        Sapi sapi10 = new Sapi();
+        
+        sapi1.warna = "Coklat";
+        sapi1.umur = 5;
+        sapi1.printStates();
+        sapi1.makan();
+        
+        sapi2.warna = "Hitam";
+        sapi2.umur = 4;
+        sapi2.printStates();
+        sapi2.tidur();
+        
+        sapi3.warna = "Putih";
+        sapi3.umur = 3;
+        sapi3.printStates();
+        sapi3.tidur();
+        
+        sapi4.warna = "Coklat";
+        sapi4.umur = 2;
+        sapi4.printStates();
+        sapi4.makan();
+        
+        sapi5.warna = "Putih";
+        sapi5.umur = 6;
+        sapi5.printStates();
+        sapi5.tidur();
+        
+        sapi6.warna = "Hitam";
+        sapi6.umur = 7;
+        sapi6.printStates();
+        sapi6.tidur();
+        
+        sapi7.warna = "Hitam";
+        sapi7.umur = 4;
+        sapi7.printStates();
+        sapi7.tidur();
+        
+        sapi8.warna = "Putih";
+        sapi8.umur = 3;
+        sapi8.printStates();
+        sapi8.tidur();
+        
+        sapi9.warna = "Coklat";
+        sapi9.umur = 2;
+        sapi9.printStates();
+        sapi9.makan();
+        
+        sapi10.warna = "Putih";
+        sapi10.umur = 6;
+        sapi10.printStates();
+        sapi10.tidur();
+        
+        Ikan ikan1 = new Ikan();
+        Ikan ikan2 = new Ikan();
+        Ikan ikan3 = new Ikan();
+        Ikan ikan4 = new Ikan();
+        Ikan ikan5 = new Ikan();
+        Ikan ikan6 = new Ikan();
+        Ikan ikan7 = new Ikan();
+        Ikan ikan8 = new Ikan();
+        Ikan ikan9 = new Ikan();
+        Ikan ikan10 = new Ikan();
+        
+        ikan1.jenis = "Hiu";
+        ikan1.warna = "Putih";
+        ikan1.printStates();
+        ikan1.makan();
+        
+        ikan2.jenis = "Lumba-lumba";
+        ikan2.warna = "Biru";
+        ikan2.printStates();
+        ikan2.makan();
+        
+        ikan3.jenis = "Orca";
+        ikan3.warna = "Hitam";
+        ikan3.printStates();
+        ikan3.tidur();
+        
+        ikan4.jenis = "Badut";
+        ikan4.warna = "Merah";
+        ikan4.printStates();
+        ikan4.makan();
+        
+        ikan5.jenis = "Sori";
+        ikan5.warna = "Putih";
+        ikan5.printStates();
+        ikan5.makan();
+        
+        ikan6.jenis = "Pari";
+        ikan6.warna = "Hitam";
+        ikan6.printStates();
+        ikan6.tidur();
+        
+        ikan7.jenis = "Buntal";
+        ikan7.warna = "Kuning";
+        ikan7.printStates();
+        ikan7.makan();
+        
+        ikan8.jenis = "Guppi";
+        ikan8.warna = "Merah";
+        ikan8.printStates();
+        ikan8.makan();
+        
+        ikan9.jenis = "Laga";
+        ikan9.warna = "Hitam";
+        ikan9.printStates();
+        ikan9.tidur();
+        
+        ikan10.jenis = "Laga";
+        ikan10.warna = "Hijau Toska";
+        ikan10.printStates();
+        ikan10.makan();
+        
+        Siswa siswa1 = new Siswa();
+        Siswa siswa2 = new Siswa();
+        Siswa siswa3 = new Siswa();
+        Siswa siswa4 = new Siswa();
+        Siswa siswa5 = new Siswa();
+        Siswa siswa6 = new Siswa();
+        Siswa siswa7 = new Siswa();
+        Siswa siswa8 = new Siswa();
+        Siswa siswa9 = new Siswa();
+        Siswa siswa10 = new Siswa();
+        
+        siswa1.nama = "Arham";
+        siswa1.kelas = "7 A";
+        siswa1.printStates();
+        siswa1.belajar();
+        
+        siswa2.nama = "Miky";
+        siswa2.kelas = "7 B";
+        siswa2.printStates();
+        siswa2.kerjaTugas();
+        
+        siswa3.nama = "Faisal";
+        siswa3.kelas = "7 C";
+        siswa3.printStates();
+        siswa3.belajar();
+        
+        siswa4.nama = "Reski";
+        siswa4.kelas = "7 D";
+        siswa4.printStates();
+        siswa4.kerjaTugas();
+        
+        siswa5.nama = "Suardi";
+        siswa5.kelas = "7 A";
+        siswa5.printStates();
+        siswa5.belajar();
+        
+        siswa6.nama = "Aiman";
+        siswa6.kelas = "7 E";
+        siswa6.printStates();
+        siswa6.kerjaTugas();
+        
+        siswa7.nama = "Shabir";
+        siswa7.kelas = "7 B";
+        siswa7.printStates();
+        siswa7.belajar();
+        
+        siswa8.nama = "Raffi";
+        siswa8.kelas = "7 C";
+        siswa8.printStates();
+        siswa8.kerjaTugas();
+        
+        siswa9.nama = "Alvin";
+        siswa9.kelas = "7 D";
+        siswa9.printStates();
+        siswa9.belajar();
+        
+        siswa10.nama = "Aril";
+        siswa10.kelas = "7 E";
+        siswa10.printStates();
+        siswa10.kerjaTugas();
+        
+        Guru guru1 = new Guru();
+        Guru guru2 = new Guru();
+        Guru guru3 = new Guru();
+        Guru guru4 = new Guru();
+        Guru guru5 = new Guru();
+        Guru guru6 = new Guru();
+        Guru guru7 = new Guru();
+        Guru guru8 = new Guru();
+        Guru guru9 = new Guru();
+        Guru guru10 = new Guru();
+        
+        guru1.nama = "Arham";
+        guru1.usia = 27;
+        guru1.printStates();
+        guru1.mengajar();
+        
+        guru2.nama = "Miky";
+        guru2.usia = 29;
+        guru2.printStates();
+        guru2.istirahat();
+        
+        guru3.nama = "Faisal";
+        guru3.usia = 28;
+        guru3.printStates();
+        guru3.mengajar();
+        
+        guru4.nama = "Reski";
+        guru4.usia = 28;
+        guru4.printStates();
+        guru4.istirahat();
+        
+        guru5.nama = "Suardi";
+        guru5.usia = 29;
+        guru5.printStates();
+        guru5.mengajar();
+        
+        guru6.nama = "Aiman";
+        guru6.usia = 29;
+        guru6.printStates();
+        guru6.istirahat();
+        
+        guru7.nama = "Shabir";
+        guru7.usia = 28;
+        guru7.printStates();
+        guru7.mengajar();
+        
+        guru8.nama = "Raffi";
+        guru8.usia = 29;
+        guru8.printStates();
+        guru8.istirahat();
+        
+        guru9.nama = "Alvin";
+        guru9.usia = 29;
+        guru9.printStates();
+        guru9.mengajar();
+        
+        guru10.nama = "Aril";
+        guru10.usia = 28;
+        guru10.printStates();
+        guru10.istirahat();
+        
+        Dosen dosen1 = new Dosen();
+        Dosen dosen2 = new Dosen();
+        Dosen dosen3 = new Dosen();
+        Dosen dosen4 = new Dosen();
+        Dosen dosen5 = new Dosen();
+        Dosen dosen6 = new Dosen();
+        Dosen dosen7 = new Dosen();
+        Dosen dosen8 = new Dosen();
+        Dosen dosen9 = new Dosen();
+        Dosen dosen10 = new Dosen();
+        
+        dosen1.nama = "Arham";
+        dosen1.usia = 27;
+        dosen1.printStates();
+        dosen1.mengajar();
+        
+        dosen2.nama = "Miky";
+        dosen2.usia = 29;
+        dosen2.printStates();
+        dosen2.istirahat();
+        
+        dosen3.nama = "Faisal";
+        dosen3.usia = 28;
+        dosen3.printStates();
+        dosen3.mengajar();
+        
+        dosen4.nama = "Reski";
+        dosen4.usia = 28;
+        dosen4.printStates();
+        dosen4.istirahat();
+        
+        dosen5.nama = "Suardi";
+        dosen5.usia = 29;
+        dosen5.printStates();
+        dosen5.mengajar();
+        
+        dosen6.nama = "Aiman";
+        dosen6.usia = 29;
+        dosen6.printStates();
+        dosen6.istirahat();
+        
+        dosen7.nama = "Shabir";
+        dosen7.usia = 28;
+        dosen7.printStates();
+        dosen7.mengajar();
+        
+        dosen8.nama = "Raffi";
+        dosen8.usia = 29;
+        dosen8.printStates();
+        dosen8.istirahat();
+        
+        dosen9.nama = "Alvin";
+        dosen9.usia = 29;
+        dosen9.printStates();
+        dosen9.mengajar();
+        
+        dosen10.nama = "Aril";
+        dosen10.usia = 28;
+        dosen10.printStates();
+        dosen10.istirahat();
+        
+        Satpam satpam1 = new Satpam();
+        Satpam satpam2 = new Satpam();
+        Satpam satpam3 = new Satpam();
+        Satpam satpam4 = new Satpam();
+        Satpam satpam5 = new Satpam();
+        Satpam satpam6 = new Satpam();
+        Satpam satpam7 = new Satpam();
+        Satpam satpam8 = new Satpam();
+        Satpam satpam9 = new Satpam();
+        Satpam satpam10 = new Satpam();
+        
+        satpam1.nama = "Arham";
+        satpam1.usia = 27;
+        satpam1.printStates();
+        satpam1.ronda();
+        
+        satpam2.nama = "Miky";
+        satpam2.usia = 29;
+        satpam2.printStates();
+        satpam2.istirahat();
+        
+        satpam3.nama = "Faisal";
+        satpam3.usia = 28;
+        satpam3.printStates();
+        satpam3.ronda();
+        
+        satpam4.nama = "Reski";
+        satpam4.usia = 28;
+        satpam4.printStates();
+        satpam4.istirahat();
+        
+        satpam5.nama = "Suardi";
+        satpam5.usia = 29;
+        satpam5.printStates();
+        satpam5.ronda();
+        
+        satpam6.nama = "Aiman";
+        satpam6.usia = 29;
+        satpam6.printStates();
+        satpam6.istirahat();
+        
+        satpam7.nama = "Shabir";
+        satpam7.usia = 28;
+        satpam7.printStates();
+        satpam7.ronda();
+        
+        satpam8.nama = "Raffi";
+        satpam8.usia = 29;
+        satpam8.printStates();
+        satpam8.istirahat();
+        
+        satpam9.nama = "Alvin";
+        satpam9.usia = 29;
+        satpam9.printStates();
+        satpam9.ronda();
+        
+        satpam10.nama = "Aril";
+        satpam10.usia = 28;
+        satpam10.printStates();
+        satpam10.istirahat();
+    }
+}
