632cd541540ac3157c7d906fab69e5c46ee63c63
Arhamullah Kamaruddin <123912972+arhamullahkamaruddin@users.noreply.github.com>
2024-03-03 21:02:25 +0800

    Class Siswa

---
+++ Siswa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tugas1;
+
+/**
+ *
+ * @author Asus
+ */
+public class Satpam {
+    String nama;
+    int usia;
+    
+    void ronda(){
+        System.out.println("Satpam sedang meronda\n");
+    }
+    
+    void istirahat(){
+        System.out.println("Satpam sedang istirahat\n");
+    }
+    
+    void printStates(){
+        System.out.println("Object satpam");
+        System.out.println("Nama: " + nama);
+        System.out.println("Usia: " + usia + " tahun");
+    }
+}
